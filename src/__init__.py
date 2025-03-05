8ac9574b69679ea4c4f5a930a556bb0ed41bbd04
Clarissa Forbes <53020872+caforbes@users.noreply.github.com>
2025-03-05 15:35:26 -0700

    Fix parser configuration loading

--- src/__init__.py
+++ src/__init__.py
@@ -2,4 +2,4 @@ STEM_PAT = r"[\w'\$]+\+[A-Z]+"
 # combo of letters, apostr, stress marker
 # +ABRV tag for part of speech
 
-from .parser import Parser, BASIC_E, BASIC_EW, FULL_E, FULL_EW
+from .parser import Parser, FULL_GIT, FULL_SGX
