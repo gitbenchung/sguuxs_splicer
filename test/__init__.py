49202dbe2ac9a7f3a36c695fcadc4827bcbae898
Clarissa Forbes <53020872+caforbes@users.noreply.github.com>
2025-03-11 16:32:27 -0700

    update tests for source code (not full fst)

--- test/__init__.py
+++ test/__init__.py
@@ -1,42 +1,39 @@
-from src import Parser, BASIC_E, BASIC_EW, FULL_E, FULL_EW
+from src import Parser
 import shutil
 import json
 import unittest
 import os
 
 
-FIX_DIR = os.path.abspath(os.path.join(
-    os.path.dirname(__file__), 'fixtures'))
+FIX_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "fixtures"))
 
 
 def create_fst_config(json_path: str, dictionary: dict) -> dict:
     with open(json_path) as f:
         config = json.load(f)
-    config['test'] = True
+    config["test"] = True
     if dictionary:
-        config['dictionary'] = dictionary
-    config['name'] = 'testy'
+        config["dictionary"] = dictionary
+    config["name"] = "testy"
     return config
 
 
 def import_fst_files(config) -> list:
     # identify required files
-    files = [f for f in config['rules_files']]
-    if type(config['dictionary']) is str:
-        if not config['dictionary'][:4] == 'test':
-            files.append(config['dictionary'])
+    files = [f for f in config["rules_files"]]
+    if type(config["dictionary"]) is str:
+        if not config["dictionary"][:4] == "test":
+            files.append(config["dictionary"])
     # copy those files to fixtures directory
     new_files = []
     for file in files:
-        orig = os.path.join(os.path.dirname(__file__), '../fst', file)
-        dest = os.path.join(os.path.dirname(__file__), 'fixtures', file)
+        orig = os.path.join(os.path.dirname(__file__), "../fst", file)
+        dest = os.path.join(os.path.dirname(__file__), "fixtures", file)
         shutil.copy(orig, dest)
         new_files.append(dest)
     # copy lexc files to directory in fixtures
-    orig_lexc = orig = os.path.join(
-        os.path.dirname(__file__), '../fst/lexc')
-    dest_lexc = orig = os.path.join(
-        os.path.dirname(__file__), 'fixtures/lexc')
+    orig_lexc = orig = os.path.join(os.path.dirname(__file__), "../fst/lexc")
+    dest_lexc = orig = os.path.join(os.path.dirname(__file__), "fixtures/lexc")
     shutil.copytree(orig_lexc, dest_lexc)
 
     return new_files
@@ -47,12 +44,10 @@ def cleanup_fst_files(fixture_files) -> None:
     for file in fixture_files:
         os.remove(file)
     # remove lexc directory
-    lexc = os.path.join(
-        os.path.dirname(__file__), 'fixtures/lexc')
+    lexc = os.path.join(os.path.dirname(__file__), "fixtures/lexc")
     shutil.rmtree(lexc)
     # remove foma directory
-    foma = os.path.join(
-        os.path.dirname(__file__), 'fixtures/foma')
+    foma = os.path.join(os.path.dirname(__file__), "fixtures/foma")
     shutil.rmtree(foma)
 
 
